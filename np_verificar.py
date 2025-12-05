724b6b3e6e3d174f67c4a8c6293cd99e1c800f2e
Paulo Roberto Souza <paulo@ai-think-solutions.com.br>
2025-12-05 20:59:23 -0300

    Remove test/debug file breaking pipeline

--- np_verificar.py
+++
@@ -1,5 +0,0 @@
-# procurar qualquer atribuição a "np" no arquivo
-Select-String -Pattern '(^|\s)np\s*=' -List -CaseSensitive .\train_legal_w2v_bilstm.py
-
-# também procura "for np in" e "def np("
-Select-String -Pattern 'for\s+np\s+in|def\s+np\s*\(' -CaseSensitive .\train_legal_w2v_bilstm.py
